8ab729c17bf975d6b3bbb0e3a21f6e945514638d
MonikaToll <monika.toll@gmail.com>
2021-10-12 19:51:45 +0200

    zadanie 1

---
+++ zaliczenieKursu/src/test/java/zad1/zad1_LoginPlusAddressTEST.java
@@ -0,0 +1,10 @@
+package zad1;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/Cucumber/Features/zaliczenie")
+public class zad1_LoginPlusAddressTEST {
+}
